23d721706e9789843ab75b96f4669b7f2ecc1c8c
jesse <nitchsx@gmail.com>
2024-12-26 10:08:08 -0300

    feat: first commit

---
+++ exemplo.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Wead\DigitalCep\Search;
+
+$busca = new Search;
+
+$resultado = $busca->getAddressFromZipcode('01050020');
+
+print_r($resultado);
