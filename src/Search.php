37f90df388d610bac6753c0c1f2bc3a61fecb9bd
jesse <nitchsx@gmail.com>
2024-12-30 12:12:12 -0300

    tag -a v1.1.2 -m "feat: update at prs's"

--- src/Search.php
+++ src/Search.php
@@ -1,19 +1,15 @@
 <?php
+
     namespace Wead\DigitalCep;
 
-    class Search
+class Search
+{
+    private $url = "https://viacep.com.br/ws/";
+
+    public function getAddressFromZipcode(string $zipCode): array
     {
-        private $url = "https://viacep.com.br/ws/";
-
-        public function getAddressFromZipcode(string $zipCode):array{
-            $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
-
-            $get = file_get_contents($this->url . $zipCode . "/json");
-
-            return (array) json_decode($get);
-
-        }
-
+        $zipCode = preg_replace('/[^0-9]/im', '', $zipCode);
+        $get = file_get_contents($this->url . $zipCode . "/json");
+        return (array) json_decode($get);
     }
-
-?>
+}
